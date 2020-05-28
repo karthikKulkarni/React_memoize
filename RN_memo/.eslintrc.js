e4848689ab81b75a61b45cd4cd8e4e55a9659808
kk <karthik.k8687@gmail.com>
2020-05-28 18:55:43 +0530

    adding congifs

--- RN_memo/.eslintrc.js
+++ RN_memo/.eslintrc.js
@@ -1,6 +1,34 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  parserOptions: {
+    createDefaultProgram: true,
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: { es6: true },
+  rules: {
+    'react/state-in-constructor': [0, 'never'],
+    'react/jsx-props-no-spreading': [
+      0,
+      {
+        html: 'ignore',
+        custom: 'ignore',
+        explicitSpread: 'ignore',
+      },
+    ],
+    '@typescript-eslint/camelcase': 'off',
+  },
 };
