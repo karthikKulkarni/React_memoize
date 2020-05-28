f79500df4480d1e1c1ae8cfbd4183f59720bdfd1
kk <karthik.k8687@gmail.com>
2020-05-28 10:38:59 +0530

    Initial commit

---
+++ ReactNative/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: { es6: true },
+  rules: {
+    'react/state-in-constructor': [0, 'never'],
+    'react/jsx-props-no-spreading': [
+      0,
+      {
+        html: 'ignore',
+        custom: 'ignore',
+        explicitSpread: 'ignore',
+      },
+    ],
+  },
+};
